f187eeb1c5d7d265416c0da0b81f6a480635960f
kangiten1920 <kangiten1920@gmail.com>
2014-03-08 19:29:10 +0900

    新規作成

---
+++ pet/common/js/jquery-base.js
@@ -0,0 +1,13 @@
+$(function() {
+	// 親メニュー処理
+	$('h2').click(function() {
+		// メニュー表示/非表示
+		$(this).next('ul').slideToggle('fast');
+	});
+	// 子メニュー処理
+	$('li').click(function(e) {
+		// メニュー表示/非表示
+		$(this).children('ul').slideToggle('fast');
+		e.stopPropagation();
+	});
+}); 
